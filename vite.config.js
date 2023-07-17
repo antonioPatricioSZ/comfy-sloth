7623f21e59c1401cad8d9ba01f9db98788f7c65a
PatricioSouza <patriciozebra3@gmail.com>
2023-07-17 17:15:43 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "src/main.js", // Path to your entry point file
-    },
-  },
-  optimizeDeps: {
-    include: ["dependency-package"], // Add the package name(s) here
-  },
 });
