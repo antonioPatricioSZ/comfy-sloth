404903d01e9f8c7aefbfd02e28a0d727ee62d46c
PatricioSouza <patriciozebra3@gmail.com>
2023-07-17 17:54:55 -0300

    teste5

--- src/pages/SingleProductPage/SingleProductPage.jsx
+++ src/pages/SingleProductPage/SingleProductPage.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useProductsContext } from '../../context/products_context'
-import { single_product_url as url } from '../../utils/constants'
-import { formatPrice } from '../../utils/helpers'
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useProductsContext } from "../../context/products_context";
+import { single_product_url as url } from "../../utils/constants";
+import { formatPrice } from "../../utils/helpers";
 import {
   Loading,
   Error,
@@ -10,20 +10,26 @@ import {
   AddToCart,
   Stars,
   PageHero,
-} from '../../components'
-import { Link } from 'react-router-dom'
-import styles from './SingleProductPage.module.css'
+} from "../../components";
+import { Link } from "react-router-dom";
+import styles from "./SingleProductPage.module.css";
 
 const SingleProductPage = () => {
-
-  const { id } = useParams()
-  const { single_product_loading: loading, single_product_error: error, single_product: product, fetchSingleProduct } = useProductsContext()
+  const { id } = useParams();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
 
   useEffect(() => {
     // https://e-commerce-node-5wf2.onrender.com/api/v1/products/${id}
-    fetchSingleProduct(`https://e-commerce-node-5wf2.onrender.com/api/v1/products/${id}`)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    fetchSingleProduct(
+      `https://e-commerce-node-5wf2.onrender.com/api/v1/products/${id}`
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   // useEffect(() => {
   //   if (error) {
@@ -36,27 +42,37 @@ const SingleProductPage = () => {
   //   return <Error />
   // }
 
-
-  if(loading) {
-    return <Loading />
+  if (loading) {
+    return <Loading />;
   }
 
-  const { _id: sku, name, price, category, description, stock, images, company } = product.singleProduct || {}
+  const {
+    _id: sku,
+    name,
+    price,
+    category,
+    description,
+    stock,
+    images,
+    company,
+  } = product.singleProduct || {};
 
   return (
     <main>
       <div className={styles.images}>
         <PageHero title={name} product={product} />
       </div>
-      <div className='section section-center page'>
-        <Link to={"/products"} className={`${styles.link} btn`}>Voltar aos produtos</Link>
+      <div className="section section-center page">
+        <Link to={"/products"} className={`${styles.link} btn`}>
+          Voltar aos produtos
+        </Link>
         <div className={styles.productCenter}>
           <div className={styles.teste}>
-            <ProductImages images={images}/>
+            <ProductImages images={images} />
           </div>
           <section className={styles.content}>
             <h2>{name}</h2>
-            <Stars stars={product.mediaReviews} reviews={product.qtdReviews}/>
+            <Stars stars={product.mediaReviews} reviews={product.qtdReviews} />
             <h5 className={styles.price}>{formatPrice(price)}</h5>
             <p className={styles.desc}>{description}</p>
             <p className={styles.info}>
@@ -79,7 +95,7 @@ const SingleProductPage = () => {
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default SingleProductPage
+export default SingleProductPage;
